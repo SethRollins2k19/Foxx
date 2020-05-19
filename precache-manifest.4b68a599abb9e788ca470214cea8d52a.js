58838bc136e713c4dc59445d38a94e0b189bf0a9
SethRollins2k19 <46203101+SethRollins2k19@users.noreply.github.com>
2020-05-19 18:14:58 +0300

    init prod

---
+++ precache-manifest.4b68a599abb9e788ca470214cea8d52a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2a81f7b9bf22f52aa75bf00a615532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8f25250b2098810ca9",
+    "url": "/static/css/2.a4a6b238.chunk.css"
+  },
+  {
+    "revision": "0491ede78d4367d3ee8b",
+    "url": "/static/css/main.b725987d.chunk.css"
+  },
+  {
+    "revision": "0c8f25250b2098810ca9",
+    "url": "/static/js/2.b44b9a7d.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.b44b9a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0491ede78d4367d3ee8b",
+    "url": "/static/js/main.9577fab0.chunk.js"
+  },
+  {
+    "revision": "7a95c07254e6b745b8c6",
+    "url": "/static/js/runtime-main.014c307f.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "06b251661dab072d9a51d3371827a1fb",
+    "url": "/static/media/MyriadPro-Black.06b25166.ttf"
+  },
+  {
+    "revision": "26ace6126968ee1b77a6780f54d6581c",
+    "url": "/static/media/MyriadPro-Black.26ace612.woff"
+  },
+  {
+    "revision": "d5b486487376f7295854815db1a93224",
+    "url": "/static/media/MyriadPro-Bold.d5b48648.ttf"
+  },
+  {
+    "revision": "eeca475980c08ee2192041c6b22c8704",
+    "url": "/static/media/MyriadPro-Bold.eeca4759.woff"
+  },
+  {
+    "revision": "59cf8cfe2aea3b4ef43f73991053de96",
+    "url": "/static/media/MyriadPro-Regular.59cf8cfe.woff"
+  },
+  {
+    "revision": "be82e3850974c1ccddfcf305d93ad7ce",
+    "url": "/static/media/MyriadPro-Regular.be82e385.ttf"
+  },
+  {
+    "revision": "36b5bab58a18b9c924861a4ccbf1a790",
+    "url": "/static/media/Roboto-Bold.36b5bab5.ttf"
+  },
+  {
+    "revision": "eb43b4c3b3b6cac224f76c0a524946a1",
+    "url": "/static/media/Roboto-Bold.eb43b4c3.woff"
+  },
+  {
+    "revision": "c8405cfa0df9fb2e47ef1c516cef59a8",
+    "url": "/static/media/Roboto-Light.c8405cfa.woff"
+  },
+  {
+    "revision": "d5c46b134c17b2138dcd1bb0efa67049",
+    "url": "/static/media/Roboto-Light.d5c46b13.ttf"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "878ad7158f6fa9136d796d4bad613bc2",
+    "url": "/static/media/Roboto-Regular.878ad715.woff"
+  },
+  {
+    "revision": "17b152d3dbff8b6818ec0e0ef08faa5a",
+    "url": "/static/media/Taile-Bold.17b152d3.woff"
+  },
+  {
+    "revision": "22181869727dc4b201302cc1c78f9ace",
+    "url": "/static/media/Taile-Bold.22181869.ttf"
+  },
+  {
+    "revision": "41b542e60d48fe9ce55f52cca935cf8f",
+    "url": "/static/media/Taile.41b542e6.ttf"
+  },
+  {
+    "revision": "a64eb5ef9bd4670f17a26630974e1282",
+    "url": "/static/media/Taile.a64eb5ef.woff"
+  },
+  {
+    "revision": "acbaf9048b2abe8491eab1b651c309ab",
+    "url": "/static/media/dark-roulette-blur.acbaf904.png"
+  },
+  {
+    "revision": "6221255896076895698a71b1b735b8f7",
+    "url": "/static/media/mv.62212558.png"
+  },
+  {
+    "revision": "dec82492322e378ece6855f1164b5ab1",
+    "url": "/static/media/new-game-bg.dec82492.png"
+  },
+  {
+    "revision": "1159e0eb5b65f5e39b7a1ab530d9188e",
+    "url": "/static/media/roulette-blur.1159e0eb.png"
+  },
+  {
+    "revision": "60849acb940316e6679b535346875b5c",
+    "url": "/static/media/sprite.60849acb.png"
+  },
+  {
+    "revision": "2c2a0493210f5ee353806cd8cb934f72",
+    "url": "/static/media/time-out-bg.2c2a0493.png"
+  },
+  {
+    "revision": "0f8cf7077a4b56af0711557155bf757e",
+    "url": "/static/media/yandex.0f8cf707.png"
+  }
+]);
